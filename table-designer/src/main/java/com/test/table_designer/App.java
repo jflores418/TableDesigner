b10b9f1eb21f746029e70a48b7e9ca6062d20ce0
GiovannyRoman <groman@hawk.iit.edu>
2018-06-25 12:59:36 -0500

    init

---
+++ table-designer/src/main/java/com/test/table_designer/App.java
@@ -0,0 +1,13 @@
+package com.test.table_designer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
